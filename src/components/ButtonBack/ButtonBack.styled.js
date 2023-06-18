9bd8eafeabb2c8026f8656ca6b3137c6f8a6007e
MovchanNatalia <Movchan.n2106@Gmail.com>
2023-06-18 21:44:33 +0300

    add app

---
+++ src/components/ButtonBack/ButtonBack.styled.js
@@ -0,0 +1,21 @@
+import styled from '@emotion/styled';
+import { NavLink } from 'react-router-dom';
+
+export const Button = styled(NavLink)`
+  position: absolute;
+
+  top: 800px;
+  left: 50%;
+  padding: 15px 15px;
+  border-radius: 2px;
+  background-color: #42362b;
+
+  font-size: 18px;
+  font-weight: 700;
+  color: #fff;
+
+  &:hover,
+  &:focus {
+    color: #454f83;
+  }
+`;
